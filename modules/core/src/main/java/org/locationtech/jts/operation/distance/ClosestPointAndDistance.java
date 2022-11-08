c69ee4051fe3ab0f88468430cdafefbdeaef1f1f
Rich MacDonald <macdonald.rich@gmail.com>
2022-11-08 16:31:39 -0600

    Merge various branches

--- modules/core/src/main/java/org/locationtech/jts/operation/distance/ClosestPointAndDistance.java
+++ modules/core/src/main/java/org/locationtech/jts/operation/distance/ClosestPointAndDistance.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2016 Vivid Solutions.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
 package org.locationtech.jts.operation.distance;
 
 import org.locationtech.jts.geom.Coordinate;
@@ -7,7 +18,6 @@ import org.locationtech.jts.geom.Coordinate;
  * Declared final so the runtime will optimize it away and eliminate gc.
  * 
  * @author rjm
- *
  */
 public final class ClosestPointAndDistance {
 	public final double dist;
